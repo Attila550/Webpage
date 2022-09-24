4ec165e74dbfaa5932ff8a2bb5dede74ab1095dd
Attila <73163523+Attila550@users.noreply.github.com>
2022-09-24 09:47:38 +0200

    Add files via upload

---
+++ ZProject/script.js
@@ -0,0 +1,8 @@
+function required(){
+   var empt = document.forms["form1"]["email1"].value;
+  if (empt == "" || empty == null )
+   {
+   alert("Prosím vyplňte prázdne pole");
+    return false;
+      }
+}
